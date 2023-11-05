82600f2052bdc23e0788103f4ba2dcdaf8e5849b
maniruzzaman-riad <monirozzamanr94@gmail.com>
2023-11-06 01:41:29 +0600

    Create contex API and control with firebase

--- src/components/Provider/AuthProvider.jsx
+++ src/components/Provider/AuthProvider.jsx
@@ -1,11 +1,57 @@
+import PropTypes from 'prop-types';
+import { createContext, useEffect, useState } from 'react';
+import auth from '../firebase/firebase.config';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 
 
-const AuthProvider = () => {
+const provider = new GoogleAuthProvider()
+export const AuthContext = createContext(null)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loding, setLoding] = useState(true)
+
+    const userRegistration = (email, password) => {
+        setLoding(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const userLogin = (email, password) => {
+        setLoding(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const userLogout = () => {
+        setLoding(true)
+        return signOut(auth)
+    }
+
+    const userSignInGoogle = () => {
+        setLoding(true)
+        return signInWithPopup(auth, provider)
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoding(false)
+        });
+        return () => {
+            unSubscribe()
+        }
+    }, [])
+
+
+
+    const authInfo = {user,loding,userRegistration,userLogin,userLogout,userSignInGoogle}
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
 
 export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './components/Routs/AllRouters.jsx'
+import AuthProvider from './components/Provider/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
